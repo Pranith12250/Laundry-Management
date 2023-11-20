e699a51c373c29cf6d2cebdfaad973f611d265fb
Pranith <spranith2074@gmail.com>
2023-11-20 11:51:58 +0000

    Laundry worker UI completed

--- src/routes/laundryworker/+page.svelte
+++ src/routes/laundryworker/+page.svelte
@@ -126,22 +126,35 @@
 <main style="background-color:#f7f3e9">
   <a href='/'>Home</a>
   <h1 style="font-size:60px">Welcome, Laundry Worker!</h1>
-  <label for="registrationNumber">Enter Registration Number:</label>
+  <label for="registrationNumber"style="font-size: 30px">Enter Student Registration Number:</label>
   <input type="text" id="registrationNumber" bind:value={registrationNumber} />
-  <button on:click={handleFetchStudentData}>Fetch Student Name</button>
+  <div class="button-container">
+    <button on:click={handleFetchStudentData}>
+      <span style="font-size: 30px;">Fetch Student data </span>
+    </button>
+  </div>
 
   {#if showData}
-    <p> <br>Student data:<br> Name: {studentName} <br></p>
-    <p> Number of washes remaining: {numberOfWashes}</p>
-    <p> Current laundry status: {status}</p>
+    <h1 style="font-style:20px"><br>Your data:<br></h1>
+    <h2 style="font-style:20px">Name: {studentName} <br></h2>
+    <h2 style="font-style:20px"> Number of washes remaining: {numberOfWashes}</h2>
+    <h2 style="font-style:20px"> Current laundry status: {status}</h2>
   {/if}
 
   {#if showData && numberOfWashes<4}
-    <button on:click={handleResetWashCycle}>Reset Wash Cycle</button>
+    <div class="button-container">
+      <button on:click={handleResetWashCycle}>
+        <span style="font-size: 20px;">Reset wash cycle </span>
+      </button>
+    </div>
   {/if}
 
   {#if showData && status=='in wash'}
-    <button on:click={handleStatusChange}>Change wash status</button>
+    <div class="button-container">
+      <button on:click={handleStatusChange}>
+        <span style="font-size: 20px;">Change wash status</span>
+      </button>
+    </div>
   {/if}
 </main>
 
@@ -153,5 +166,58 @@
     align-items: center;
     justify-content: center;
     height: 100vh;
+    overflow: auto;
+    padding-top: 50px;
+    padding-bottom: 50px;
+  }
+
+  button
+  {
+    border-radius: 20px;
+    background-color: #3498db;
+    transition: background-color 0.3s, transform 0.2s;
+  }
+  button:hover 
+  {
+    background-color: #e06b48;
+    transform: scale(1.05);
+  }
+  button:active 
+  {
+    transform: scale(0.95);
+  }
+  .button-container 
+  {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+  }
+
+  input[type="text"] 
+  {
+    margin-bottom: 20px;
+    padding: 10px;
+    border: 2px solid #3498db;
+    border-radius: 10px;
+    transition: border-color 0.3s, transform 0.2s;
+    font-size: 18px;
+  }
+  input[type="text"]:hover 
+  {
+    border-color: #e06b48;
+    transform: scale(1.05);
+  }
+  input[type="text"]:focus 
+  {
+    outline: none;
+    border-color: #e06b48;
+    transform: scale(1.05);
+  }
+
+  label 
+  {
+    margin-bottom: 10px;
+    font-size: 30px;
   }
 </style>
